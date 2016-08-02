b52195c2a91f940fd4770f013a671f0f45f17b0e
Brett New and Kevin VanEvery <brett+kevin@>
2016-08-02 14:40:20 -0700

    add functionality to allow user to display different degrees of weather based on the buttons.

--- js/temperature-interface.js
+++ js/temperature-interface.js
@@ -3,13 +3,19 @@ var KelvinToFahrenheit = require('./../js/temperature-model.js').KelvinToFahrenh
 
 
 $(document).ready(function(){
-  $("#temperature").click(function(){
+  $("button.btn").click(function(){
+    var tempScale = $(this).val();
+    // debugger;
     var city = $('#location').val();
     $('#location').val("");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('.showTemperature').append("The temperature in " + city + " is " + Math.floor((parseInt(response.main.temp))) + " degrees Kelvin</br>");
-      $('.showTemperature').append("The temperature in " + city + " is " + Math.floor(KelvinToCelsius(parseInt(response.main.temp))) + " degrees Celsius</br>");
-      $('.showTemperature').append("The temperature in " + city + " is " + Math.floor(KelvinToFahrenheit(parseInt(response.main.temp))) + " degrees Fahrenheit</br>");
+      if (tempScale === "kelvin") {
+        $('.showTemperature').append("The temperature in " + city + " is " + Math.floor((parseInt(response.main.temp))) + " degrees Kelvin</br>");
+      } else if (tempScale === "celsius") {
+        $('.showTemperature').append("The temperature in " + city + " is " + Math.floor(KelvinToCelsius(parseInt(response.main.temp))) + " degrees Celsius</br>");
+      } else {
+        $('.showTemperature').append("The temperature in " + city + " is " + Math.floor(KelvinToFahrenheit(parseInt(response.main.temp))) + " degrees Fahrenheit</br>");
+      }
     }).fail(function(error){
       $('.showTemperature').text(error.statusText);
     });
